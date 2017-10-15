87611afa1ed4f927446ded919696b65f584f1fe3
Yan Gao <yan.gao@darkmatter.ae>
2017-10-15 08:56:16 -0400

    my first commit

---
+++ src/test/java/Example/NewTest.java
@@ -0,0 +1,31 @@
+package Example;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/selenium/guru99home/");  
+			String title = driver.getTitle();				 
+			AssertJUnit.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
+
