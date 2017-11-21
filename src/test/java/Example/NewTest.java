0b468467f6be0f1d04007a10383a6c6a872705c7
Yan Gao <yan.gao@darkmatter.ae>
2017-11-21 08:04:05 -0500

    add chrome driver

--- src/test/java/Example/NewTest.java
+++ src/test/java/Example/NewTest.java
@@ -13,22 +13,31 @@ import org.testng.Assert;
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NewTest {		
-	    private WebDriver driver;		
-		@Test				
-		public void testEasy() {	
-			driver.get("http://demo.guru99.com/selenium/guru99home/");  
-			String title = driver.getTitle();				 
-			AssertJUnit.assertTrue(title.contains("Demo Guru99 Page")); 		
-		}	
-		@BeforeTest
-		public void beforeTest() {	
-		    driver = new FirefoxDriver();  
-		}		
-		@AfterTest
-		public void afterTest() {
-			driver.quit();			
-		}		
-}	
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
+public class NewTest {
+	public static String driverPath = "/usr/local/bin/";
+	public static WebDriver driver;
+  @Test
+  public void main() {
+		driver.navigate().to("http://google.com");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver");
+		driver = new ChromeDriver();
+
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
